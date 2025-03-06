b1d3a3852db1eb62f2579166a2a8378e52c86e38
Aman Mohamed Jh <167648009+AmanMohamedJh@users.noreply.github.com>
2025-03-06 20:49:07 +0530

    First base commit by Aman

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const App = express();
+require("dotenv").config();
+
+//middlewares
+
+//Routes
+
+//listen for request
+
+//listen for request
+
+App.listen(process.env.PORT, () => {
+  console.log("Listening to the port : ", process.env.PORT);
+});
